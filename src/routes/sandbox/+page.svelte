3e8de86ee3eba1e3b199923b37af56bfd975146f
Scott Silsbe <sils@impossible-bureau.com>
2023-01-30 16:04:33 -0500

    add component to sandbox

--- src/routes/sandbox/+page.svelte
+++ src/routes/sandbox/+page.svelte
@@ -1,45 +1,9 @@
 <!-- sandbox -->
 <script lang="ts">
-  import { onMount } from 'svelte';
-
-  let canvas: HTMLCanvasElement;
-
-  onMount(() => {
-    const ctx = canvas.getContext('2d');
-    let frame = requestAnimationFrame(loop);
-
-    function loop(t: number) {
-      if (ctx === null) return;
-
-      frame = requestAnimationFrame(loop);
-
-      const imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
-
-      for (let p = 0; p < imageData.data.length; p += 4) {
-        const i = p / 4;
-        const x = i % canvas.width;
-        const y = (i / canvas.width) >>> 0;
-
-        const r = 64 + (128 * x) / canvas.width + 64 * Math.sin(t / 1000);
-        const g = 64 + (128 * y) / canvas.height + 64 * Math.cos(t / 1000);
-        const b = 128;
-
-        imageData.data[p + 0] = r;
-        imageData.data[p + 1] = g;
-        imageData.data[p + 2] = b;
-        imageData.data[p + 3] = 255;
-      }
-
-      ctx.putImageData(imageData, 0, 0);
-    }
-
-    return () => {
-      cancelAnimationFrame(frame);
-    };
-  });
+  import FlipForm from '$components/sandbox/FlipForm.svelte';
 </script>
 
-<div><canvas bind:this={canvas} width={600} height={32} /></div>
+<div><FlipForm /></div>
 
 <style lang="scss">
   div {
@@ -50,18 +14,5 @@
 
     height: 100%;
     width: 100%;
-
-    padding: 50px;
-
-    background-color: var(--black);
-  }
-
-  canvas {
-    width: 100%;
-    height: 100%;
-    background-color: rgb(255, 255, 255);
-    -webkit-mask: url('$assets/images/closed-system-title.svg') 50% 50%
-      no-repeat;
-    mask: url('$assets/images/closed-system-title.svg') 50% 50% no-repeat;
   }
 </style>
