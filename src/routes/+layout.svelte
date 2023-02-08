4d24d408d8d3d94d42b8ec14cbde9a445f18ec04
Scott Silsbe <sils@impossible-bureau.com>
2023-02-08 11:19:25 -0500

    get/parse json data from contentful data

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-  import type { PageId, Quote } from '$lib/types';
+  import type { PageId, Quote, ContentfulData } from '$lib/types';
   import type { LayoutData } from './$types';
   import { page } from '$app/stores';
   import Header from '$components/Header.svelte';
@@ -18,8 +18,16 @@
   let quote: Quote;
   let pageId: PageId = setInitialPageId($page?.route?.id);
 
+  const getMasterJSON = (data: ContentfulData) => {
+    const item = data?.items?.find(
+      (item: any) => item?.fields?.fid === 'content master json',
+    );
+    return item?.fields?.webAllContent;
+  };
+
   $: ({ contentfulData, jsioRecord, themeCookie } = data);
-  $: content.set({ ...jsioRecord, contentfulData });
+  $: contentfulJSON = getMasterJSON(contentfulData);
+  $: content.set({ ...jsioRecord, contentfulData, contentfulJSON });
   $: theme.set(themeCookie || dark);
   $: quotes = jsioRecord.quotes.quotes;
   $: quote = sample(quotes);
