ea6a76c935a3e98b21e9f9999a65b596bc6fa2a1
Scott Silsbe <sils@impossible-bureau.com>
2023-02-19 15:43:13 -0500

    add gallery component

---
+++ src/components/Gallery.svelte
@@ -0,0 +1,20 @@
+<!-- image gallery -->
+<script lang="ts">
+  import type { ImageData } from '$lib/types';
+  import '$styles/components/gallery.scss';
+
+  export let images: ImageData[];
+
+  $: setClassesFromTags = (tags: string[] | undefined) => {
+    if (!tags) return '';
+    return tags.toString().replaceAll(',', ' ');
+  };
+</script>
+
+<ul class="img-gallery">
+  {#each images as { url, title, alt, tags }}
+    <li class={setClassesFromTags(tags)}>
+      <img src={url} {title} {alt} />
+    </li>
+  {/each}
+</ul>
