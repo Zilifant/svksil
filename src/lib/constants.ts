abf96bb2654bc3bac009f9a77cd0f37ae4aa38d4
Scott Silsbe <sils@impossible-bureau.com>
2023-02-23 14:35:18 -0500

    add design page

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -2,7 +2,7 @@
 
 import type { Page } from '$lib/types';
 
-export const version = '2.0.0';
+export const version = '2.1.2';
 
 export const dark = 'dark';
 export const light = 'light';
@@ -12,12 +12,14 @@ export const pages: Page[] = [
   { id: 'bio',     href: '/',        color: 'pink',   pos: 1 },
   { id: 'resume',  href: '/resume',  color: 'blue',   pos: 2 },
   { id: 'code',    href: '/code',    color: 'green',  pos: 3 },
-  { id: 'writing', href: '/writing', color: 'orange', pos: 4 },
+  { id: 'design',  href: '/design',  color: 'purple', pos: 4 },
+  { id: 'writing', href: '/writing', color: 'orange', pos: 5 },
 ];
 
 export const bio = 'bio';
 export const res = 'resume';
 export const code = 'code';
+export const desi = 'design';
 export const writ = 'writing';
 
 export const pageIds = [...pages.map((page) => page.id)];
