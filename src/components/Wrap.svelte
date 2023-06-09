a92849ee11d185034560d47fc584a2d422ecadba
Scott Silsbe <sils@impossible-bureau.com>
2023-06-09 12:22:55 -0400

    refactor to fix UI bug on first load+page switch

--- src/components/Wrap.svelte
+++ src/components/Wrap.svelte
@@ -1,5 +1,5 @@
 <!--
-When switching pages, previous page's content fades out while new page's content flys in
+When switching pages, previous page's content fades out while new page's content flies in
 from right or left, depending on the pages' relative positions in the navbar.
 -->
 <script lang="ts">
@@ -7,7 +7,12 @@ from right or left, depending on the pages' relative positions in the navbar.
   import { fly, fade } from 'svelte/transition';
 
   const flyDistance = 3000;
-  const flyVector = $animation === 'flyLeft' ? flyDistance : -flyDistance;
+  const flyVector =
+    $animation === 'flyLeft'
+      ? flyDistance
+      : $animation === 'flyRight'
+      ? -flyDistance
+      : 0;
   const flyInDelay = 0;
   const flyInDuration = 500;
 
@@ -18,10 +23,10 @@ from right or left, depending on the pages' relative positions in the navbar.
 
 <!-- Wrapper -->
 
-{#if $animation === 'fade'}
+{#if $animation === 'flyLeft' || $animation === 'flyRight'}
   <div
     class="transition-wrapper"
-    in:fade={{ delay: fadeInDelay, duration: fadeInDuration }}
+    in:fly={{ x: flyVector, delay: flyInDelay, duration: flyInDuration }}
     out:fade={{ duration: fadeOutDuration }}
   >
     <slot />
@@ -29,7 +34,7 @@ from right or left, depending on the pages' relative positions in the navbar.
 {:else}
   <div
     class="transition-wrapper"
-    in:fly={{ x: flyVector, delay: flyInDelay, duration: flyInDuration }}
+    in:fade={{ delay: fadeInDelay, duration: fadeInDuration }}
     out:fade={{ duration: fadeOutDuration }}
   >
     <slot />
