11b5aa9cd8fcb2dab0b2964e474963e9adec230d
Zilifant <scott.silsbe@gmail.com>
2023-12-04 16:10:12 -0800

    remove code page

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -12,7 +12,7 @@ export const pages: Page[] = [
   { id: 'bio',     href: '/',        color: 'pink',   pos: 1 },
   { id: 'resume',  href: '/resume',  color: 'blue',   pos: 2 },
   { id: 'code',    href: '/code',    color: 'green',  pos: 3 },
-  { id: 'blog',    href: '/blog',    color: 'red',    pos: 4 },
+  // { id: 'blog',    href: '/blog',    color: 'red',    pos: 4 },
   { id: 'design',  href: '/design',  color: 'purple', pos: 5 },
   { id: 'writing', href: '/writing', color: 'orange', pos: 6 },
 ];
