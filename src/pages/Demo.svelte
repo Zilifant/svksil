d95f48e767184d6cbb6f0178bf1713d7fd7bb9c5
Zilifant <scott.silsbe@gmail.com>
2024-04-10 18:00:49 -0700

    coding task

---
+++ src/pages/Demo.svelte
@@ -0,0 +1,110 @@
+<!-- Sandbox -->
+<script lang="ts">
+  import '$styles/pages/demo.scss';
+
+  const QUOTE = `All the world's a stage,
+And all the men and women merely players;
+They have their exits and their entrances;
+And one man in his time plays many parts,
+His acts being seven ages. At first the infant,
+Mewling and puking in the nurse's arms;
+And then the whining school-boy, with his satchel
+And shining morning face, creeping like snail
+Unwillingly to school. And then the lover,
+Sighing like furnace, with a woeful ballad
+Made to his mistress' eyebrow. Then a soldier,
+Full of strange oaths, and bearded like the pard,
+Jealous in honour, sudden and quick in quarrel,
+Seeking the bubble reputation
+Even in the cannon's mouth. And then the justice,
+In fair round belly with good capon lin'd,
+With eyes severe and beard of formal cut,
+Full of wise saws and modern instances;
+And so he plays his part. The sixth age shifts
+Into the lean and slipper'd pantaloon,
+With spectacles on nose and pouch on side;
+His youthful hose, well sav'd, a world too wide
+For his shrunk shank; and his big manly voice,
+Turning again toward childish treble, pipes
+And whistles in his sound. Last scene of all,
+That ends this strange eventful history,
+Is second childishness and mere oblivion;
+Sans teeth, sans eyes, sans taste, sans everything`;
+
+  let inputText = '';
+
+  const getQuote = (): string => {
+    // const element = document.querySelector('#quote');
+    // // @ts-ignore
+    // const quote = element?.value;
+    // return quote;
+    return inputText;
+  };
+
+  const tallyModifications = (quote: string): { modifications: number } => {
+    const regex = /( his )|(His)|('big manly voice', 'assertive voice')/g;
+    const tally = (quote.match(regex) || []).length;
+    return {
+      modifications: tally,
+    };
+  };
+
+  const updateQuote = (quote: string): string => {
+    return quote
+      .replaceAll(' his ', ' his/her ')
+      .replaceAll('His', 'His/Her')
+      .replace('big manly voice', 'assertive voice');
+  };
+
+  const displayUpdatedQuote = (updatedQuote: string): void => {
+    const element = document.querySelector('.output');
+    if (element) {
+      element.textContent = updatedQuote;
+      element.classList.remove('untouched');
+    }
+  };
+
+  const displayTally = (tally: string): void => {
+    const element = document.querySelector('.tally');
+    if (element) {
+      element.textContent = tally.toString();
+      element.classList.remove('untouched');
+    }
+  };
+
+  const updateQuoteAndDisplay = (): void => {
+    const quote = getQuote();
+    if (!quote) return;
+
+    const updatedQuote = updateQuote(quote);
+    displayUpdatedQuote(updatedQuote);
+    const tally = tallyModifications(quote);
+    displayTally(JSON.stringify(tally));
+
+    inputText = '';
+  };
+</script>
+
+<div class="page-wrapper demo">
+  <div class="text-replacer-wrapper">
+    <form class="text-replacer-form">
+      <div class="input-wrapper">
+        <label for="text">Enter Quote: </label>
+        <textarea bind:value={inputText} id="quote" required />
+      </div>
+      <button class="submit-button" on:click={updateQuoteAndDisplay}>
+        <p>Update Quote</p>
+      </button>
+    </form>
+    <div class="outputs-wrapper">
+      <div class="output-wrapper">
+        <h6>New Quote:</h6>
+        <p class="output untouched">Enter a quote above.</p>
+      </div>
+      <div class="tally-wrapper">
+        <h6>Modification Tally:</h6>
+        <p class="tally untouched">Enter a quote above.</p>
+      </div>
+    </div>
+  </div>
+</div>
---
+++ src/routes/demo/+page.svelte
@@ -0,0 +1,7 @@
+<!-- sandbox -->
+<script lang="ts">
+  import Wrap from '$components/Wrap.svelte';
+  import Demo from '$pages/Demo.svelte';
+</script>
+
+<Wrap><Demo /></Wrap>
